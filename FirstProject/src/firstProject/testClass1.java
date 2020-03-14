01deadf84d4d29800bd130ba1507ed505f90cc7e
selva <selva@Shanuka-Lakshanashri>
2020-03-14 07:38:49 -0500

    Added Comment

--- FirstProject/src/firstProject/testClass1.java
+++ FirstProject/src/firstProject/testClass1.java
@@ -6,16 +6,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class testClass1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
+		// Webdriver path
 		System.setProperty("webdriver.chrome.driver", "C:\\Software\\chromedriver_win32\\chromedriver.exe");
 		
+		//driver object
 		WebDriver driver = new ChromeDriver();
 		
+		//Open google.com
 		driver.get("http://www.google.com/");
 		
+		//Print current url
 		System.out.println(driver.getCurrentUrl());
 		
+		//Get current page title
 		System.out.println(driver.getTitle());
 		//driver.close();
 		
